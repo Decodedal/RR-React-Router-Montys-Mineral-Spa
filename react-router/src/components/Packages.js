f276532394c1d99d89fa4e70bc9cffaf2d7c9c6c
Decodedal <Palumbodallas@gmail.com>
2022-09-12 20:26:45 -0400

    routed

--- react-router/src/components/Packages.js
+++ react-router/src/components/Packages.js
@@ -1,12 +1,22 @@
 
 
-export default function Packages() {
+export default function Packages(props) {
+
+    const displayPackages = props.packages.map((pack, i) =>{
+        return(
+            <li key={i}>{pack}</li>
+        )
+     })
+    //const displayPackages = props.packages.map((eachPackage) => <li>{eachPackage}</li>)
 
     return (
         <div>
             <div className="packages">
                 <div className="packagesHeader">
                     <h1>Our Packages</h1>
+                    <ul>
+                        {displayPackages}
+                    </ul>
                 </div>
                 
             </div>
